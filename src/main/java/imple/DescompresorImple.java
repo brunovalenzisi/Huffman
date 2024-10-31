e86c682876eade4c4263b090311f8046cfecaf37
Santi <shuamani@frba.utn.edu.ar>
2024-10-31 02:08:19 -0300

    Algunos cambios en DescompresorImple

--- src/main/java/imple/DescompresorImple.java
+++ src/main/java/imple/DescompresorImple.java
@@ -3,14 +3,32 @@ package imple;
 import huffman.def.Descompresor;
 import huffman.def.HuffmanInfo;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class DescompresorImple implements Descompresor {
     
 
 
 	public HuffmanInfo recomponerArbol(String filename){
-        HuffmanInfo info=new HuffmanInfo();
-        return info;
+        HuffmanTable arrAux []; /*Esto debe ser de otro tipo de dato, al no poder agregarse elementos como tal a un array*/
 
+        HuffmanInfo p=new HuffmanInfo(); // acepto que es el primer puntero al arbol
+
+        try (FileInputStream fis = new FileInputStream(filename)) {
+            int data;
+            while ((data = fis.read()) != -1) {
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); // Muestra el error en la consola
+        }
+
+
+
+
+        return info;
     };
 	
 	public void descomprimirArchivo(HuffmanInfo root,String filename){
